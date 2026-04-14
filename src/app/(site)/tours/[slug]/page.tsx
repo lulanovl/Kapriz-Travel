e4b8841afd6139db8bf4878b194bfb51b07a6a52
[lulanovl] <[eldarulanov02@gmail.com]>
2026-04-14 16:49:10 +0600

    server error

--- src/app/(site)/tours/[slug]/page.tsx
+++ src/app/(site)/tours/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
